6df0d9197e38a4b4c40588521f76d63ac20b854a
Simon Brix <s.brix@eslgaming.com>
2019-03-18 13:39:38 +0100

    working version

---
+++ index.js
@@ -0,0 +1,41 @@
+function ratioToRGB ({ r, g, b }) {
+  return ({
+    r: r * 255,
+    g: g * 255,
+    b: b * 255
+  })
+}
+
+export default function getColors (document, nodeType, nameRegex) {
+  nodeType = nodeType || 'RECTANGLE'
+  nameRegex = nameRegex || /.*/g
+
+  if (!document) {
+    return null
+  }
+
+  let results = []
+
+  function visit (node) {
+    if (
+      node.name.match(nameRegex)
+      && node.type === nodeType
+      && node.fills.length
+      && node.fills[0].color
+    ) {
+      const color = ratioToRGB(node.fills[0].color)
+      const { name } = node
+
+      results = [...results, ...[{name, color}]]
+    }
+
+    if ('children' in node) {
+      node.children.forEach(visit)
+    }
+  }
+
+  visit(document)
+
+  return results
+}
+
