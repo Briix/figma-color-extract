4292acde819f2a1756b39357a3746494ef151bfb
wei <wei@searchkings.ca>
2020-02-06 09:14:39 -0500

    add channel parameter

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
-function ratioToRGB ({ r, g, b }) {
+function ratioToRGB({ r, g, b }) {
   return ({
     r: r * 255,
     g: g * 255,
-    b: b * 255
+    b: b * 255,
   })
 }
 
-export default function getColors (document, nodeType, nameRegex) {
+function ratioToRGBA({ r, g, b, a }) {
+  return ({
+    r: r * 255,
+    g: g * 255,
+    b: b * 255,
+    a: a
+  })
+}
+
+export default function getColors(document, channelType, nodeType, nameRegex) {
   nodeType = nodeType || ['RECTANGLE']
   nameRegex = nameRegex || /.*/g
+  channelType = channelType || 'rgb'
 
   if (!document) {
     return null
@@ -16,17 +26,37 @@ export default function getColors (document, nodeType, nameRegex) {
 
   let results = []
 
-  function visit (node) {
+  function visit(node) {
+
     if (
       node.name.match(nameRegex)
       && nodeType.includes(node.type)
       && node.fills.length
       && node.fills[0].color
     ) {
-      const color = ratioToRGB(node.fills[0].color)
+
+      let color;
+
+      if (channelType === 'rgba') {
+
+        let newColorObj = {
+          r: node.fills[0].color.r,
+          g: node.fills[0].color.g,
+          b: node.fills[0].color.b,
+          a: node.fills[0].opacity,
+        };
+
+        color = ratioToRGBA(newColorObj)
+
+      } else {
+
+        color = ratioToRGB(node.fills[0].color)
+
+      }
+
       const { name } = node
 
-      results = [...results, ...[{name, color}]]
+      results = [...results, ...[{ name, color }]]
     }
 
     if ('children' in node) {
